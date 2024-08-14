32811d5ff418f0ed4918f04cbb6e19f64bfba379
Jurriaan Roelofs <jur@dxpr.com>
2024-08-14 18:41:59 +0200

    style tweaks

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')
-  ]
+    require('autoprefixer'),
+    require('postcss-pxtorem')({
+      rootValue: 16,
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0,
+    }),
+  ],
 };
